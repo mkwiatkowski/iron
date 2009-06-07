31046b2875b53f4bbf7910abb51eb1aab180c8dd
Michal Kwiatkowski <constant.beta@gmail.com>
2009-06-08 01:32:39 +0200

    Implemented read-json-file using Dan Larkin's json library.

--- iron.clj
+++ iron.clj
@@ -1,8 +1,25 @@
 (ns iron
+  (:require
+   (org.danlarkin [json :as json]))
   (:import
+   (java.io FileReader)
    (java.awt GridLayout)
    (javax.swing JFrame JLabel JTextField)
    (javax.swing.event DocumentListener)))
+
+(defn call-and-measure [message fun]
+  (printf "%s... " message) (flush)
+  (let [start (. System (nanoTime))
+        ret (fun)]
+    (println
+     (format "done in %.3f seconds."
+             (/ (double (- (. System (nanoTime)) start)) 1000000000.0)))
+    ret))
+
+(defn read-json-file [filepath]
+  (call-and-measure
+   (format "Reading json file %s" filepath)
+   #(json/decode-from-reader (FileReader. filepath))))
 
 (defn document-listener [field func]
   (.addDocumentListener
@@ -31,6 +48,7 @@
   (let [frame (JFrame. "Iron")
         label (JLabel. "Hello world")
         field (JTextField. 20)]
-    (configure-gui frame label field)))
+    (configure-gui frame label field))
+  (read-json-file (second *command-line-args*)))
 
 (if *command-line-args* (main))
